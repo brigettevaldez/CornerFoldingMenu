3d4f5e6840f9252d0988ced1a29745e742a30f14
Brigette Valdez <valdez.brigette@gmail.com>
2021-03-22 13:16:36 -0500

    Initial Commit

---
+++ CornerFoldingMenu/CornerFoldingMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  CornerFoldingMenuApp.swift
+//  CornerFoldingMenu
+//
+//  Created by Brigette Valdez on 3/22/21.
+//
+
+import SwiftUI
+
+@main
+struct CornerFoldingMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
